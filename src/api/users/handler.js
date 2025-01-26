997528cda0e93e2c89a4c402c1a0d7f1269f1ea6
studiocode1 <febriardiansyah020227@gmail.com>
2025-01-26 17:42:39 +0700

    registered users implemented

---
+++ src/api/users/handler.js
@@ -0,0 +1,44 @@
+class UsersHandler {
+  constructor(validator, service) {
+    this._validator = validator;
+    this._service = service;
+
+    this.postUserHandler = this.postUserHandler.bind(this);
+    this.getUserByIdHandler = this.getUserByIdHandler.bind(this);
+  }
+
+  async postUserHandler(request, h) {
+    const { username, password, fullname } = request.payload;
+
+    const userId = await this._service.addUser({
+      username,
+      password,
+      fullname,
+    });
+
+    return h
+      .response({
+        status: 'success',
+        message: 'user berhasil ditambahkan',
+        data: {
+          userId,
+        },
+      })
+      .code(201);
+  }
+
+  async getUserByIdHandler(request, h) {
+    const { id } = request.params;
+
+    const user = await this._service.getUserById(id);
+
+    return h.response({
+      status: 'success',
+      data: {
+        user,
+      },
+    });
+  }
+}
+
+module.exports = UsersHandler;
