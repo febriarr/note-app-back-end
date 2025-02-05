a5643d69bb014c6ca553d61da3fe125d7b3a7e8c
studiocode1 <febriardiansyah020227@gmail.com>
2025-02-05 19:01:25 +0700

    authentications

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -4,6 +4,7 @@ const NotFoundError = require('../../exceptions/NotFoundError');
 const bcrypt = require('bcrypt');
 
 const { Pool } = require('pg');
+const AuthenticationError = require('../../exceptions/AuthenticationError');
 
 class UsersService {
   constructor() {
@@ -58,6 +59,29 @@ class UsersService {
     }
 
     return result.rows[0];
+  }
+
+  async verifyUserCredential(username, password) {
+    const query = {
+      text: 'SELECT id, password FROM users WHERE username = $1',
+      values: [username],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new AuthenticationError('Kredensial yang anda berikan salah');
+    }
+
+    const { id, password: hashedPassword } = result.rows[0];
+
+    const match = await bcrypt.compare(password, hashedPassword);
+
+    if (!match) {
+      throw new AuthenticationError('Kredensial yang anda berikan salah');
+    }
+
+    return id;
   }
 }
 
